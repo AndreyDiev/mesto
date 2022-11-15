f67f441b3f6079bde7d1d20ac33f062e4464b2d8
AndreyDiev <110460038+AndreyDiev@users.noreply.github.com>
2022-11-15 16:42:09 +0300

    3 try

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closePopupButton = document.querySelector('.popup__close');
+let formName = document.querySelector('.popup__input_value_name');
+let formJob = document.querySelector('.popup__input_value_job');
+let popupForm = document.querySelector('.popup__form');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+// let cards = document.querySelectorAll('.card__like');
+// likeButtonArray = Array.from(cards);
+
+// likeButtonArray.forEach((card) => {
+//  card.addEventListener('click', () => {
+//    card.classList.toggle('card__like_active');
+//  })
+// });
+
+function clickedEditButton() {
+  popup.classList.add('popup__active');
+  formName.value = profileName.textContent;
+  formJob.value = profileJob.textContent;
+}
+
+function clickedClosePopupButton() {
+  popup.classList.remove('popup__active');
+}
+
+function formSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = formName.value;
+  profileJob.textContent = formJob.value;
+  return popup.classList.remove('popup__active');
+}
+
+editButton.addEventListener('click', clickedEditButton);
+closePopupButton.addEventListener('click', clickedClosePopupButton);
+popupForm.addEventListener('submit', formSubmit);
+
